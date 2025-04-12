c1b004ebfbaae8c1db09975a3256a583447bbf63
gaboart96 <114592717+gaboart96@users.noreply.github.com>
2025-04-12 16:02:03 -0300

    Comentarios en main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const cantNombres = 2;
+    const cantNombres = 2; // se asume que hay dos nombres
     let integrante1 = null;
     let integrante2 = null;
     let form1 = document.querySelector("#form1");
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log(informarIntegrante(integrante2));
             coincidenNombres(integrante1, integrante2, 0, cantNombres); 
             if(confirm("Desea comprobar los apellidos?")){
-                coincidenNombres(integrante1, integrante2, cantNombres, valores1.length); 
+                coincidenNombres(integrante1, integrante2, cantNombres, valores1.length); // se reutiliza la función de coincidenNombres, pero en vez de ir de 0 hasta cantNombres, va desde cantNombres hasta el final del arreglo
             };
         }
         else
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function completar(form){
         return {
-            nombre1: form.elements["nombre1"].value,
+            nombre1: form.elements["nombre1"].value, // como los name de los inputs de ambos formularios son iguales, se puede usar la misma función para ambos
             nombre2: form.elements["nombre2"].value,
             apellido1: form.elements["apellido1"].value,
             apellido2: form.elements["apellido2"].value,
@@ -48,16 +48,16 @@ document.addEventListener("DOMContentLoaded", function() {
         let valores = Object.values(integrante);
         let nombreyApellido = "";
         for(let i = 0; i < valores.length ; i++){
-           nombreyApellido += valores[i];
+           nombreyApellido += valores[i]; // se crea un string al que se le van concatenando los diferentes valores del objeto integrante. esto es para que se imprima todo en una sola línea.
             if(valores[i] != ""){
-                nombreyApellido += " ";
+                nombreyApellido += " "; // si el input no es un espacio vacío, se suma un espacio al final (a fin de evitar que, si, por ejemplo, el usuario no tiene un segundo nombre o segundo apellido, se concatene un espacio extra)
             }
         }
         return nombreyApellido;
     }
 
     function coincidenNombres(integrante1, integrante2, min, max){     
-        if ((integrante1 && integrante2) && coincideNombre(integrante1, integrante2, min, max)){
+        if ((integrante1 && integrante2) && coincideNombre(integrante1, integrante2, min, max)){ 
             color = pedirColor();
             let p = document.createElement("p");
             p.textContent = "Coincidencias";
@@ -80,18 +80,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function coincideNombre(integrante1, integrante2, min, max){
-        let i = min;
+        let i = min; // se le pasa un min y un max a la funcion, para iterar nombres o apellidos según sea necesario
         while((i < max) && (!(esNombreIgual(valores1[i], valores2, max))|| (valores1[i] == ""))){
-            i++;
+            i++; // el bucle va recorriendo los valores del primer integrante hasta que encuentra coincidencias para ese valor
         }
-        return(i < max);
+        return(i < max); // si el bucle no llega hasta el final del arreglo, quiere decir que encontró coincidencias
     }
 
     function esNombreIgual(palabra1, palabras2, cant){
-        let j = 0;
+        let j = 0; // se le pasa un valor de integrante1 y recorre integrante2 hasta encontrar coincidencias
             while ((j < cant) && (palabra1 != palabras2[j]))
                 j++;
-        return (j < cant);
+        return (j < cant); // misma lógica que coincideNombre
     }
 
     function imprimirCoincidencias(integrante, color, min, max){
@@ -105,13 +105,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 coincidencias.appendChild(strong);
             }
         }      
-    }
+    } se recorren los valores de integrante y se verifica que tengan coincidencias. si las tienen, se escribe ne el html el nombre adentro de una etiqueta strong, y se modifica el atributo color en el css de strong introduciendo color ingresado por el usuario
 
     function pedirColor(){
-        const regEx = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+        const regEx = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/; // expresión regular de color en número hexadecimal
         let color = prompt("Coincide al menos un nombre. Elija un color hexadecimal para destacarlo(s).");
         while (!(regEx.test(color))){
-            color = prompt("Color inválido. Ingrese un número hexadecimal.");
+            color = prompt("Color inválido. Ingrese un número hexadecimal."); // si el color no es correcto, se le pide al usuario por prompt un número hasta que sea correcto
         }
         console.log(color);
         return color;     
@@ -124,7 +124,7 @@ document.addEventListener("DOMContentLoaded", function() {
             i++;
 
         }
-        return (i == valores.length);
+        return (i == valores.length); // se recorren los valores del objeto integrantes, verificando que todas las instancias sean letras
 
     }
 })
